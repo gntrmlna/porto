67d8a0c99cb4847da4472ab12324421642307c02
Guntur <gntrmlnafrds@gmail.com>
2023-01-06 18:55:52 +0700

    add device pic

--- src/Components/contentWelcome.jsx
+++ src/Components/contentWelcome.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import avatar from './avatar.png'
+import device from './device.png'
 export default function Welcome() {
   return (
     <>
-        <div className='text-center mt-12'>
-            <h1 className='text-4xl font-bold'>Front End Developer</h1>
-            <h1 className='text-2xl mt-8'>I love simple things, and i love what i do.</h1>
-            <div className='flex justify-center mt-12'>
-                <img className='rounded-full border' src={avatar} alt="" width={200} height={200}/>
+        <div className='container h-1/2 mx-auto'>
+          <div className='text-center mt-12'>
+              <h1 className='text-4xl font-bold'>Front End Developer</h1>
+              <h1 className='text-2xl mt-8'>I love simple things, and i love what i do.</h1>
+              <div className='flex justify-center mt-20'>
+                  <img className='rounded-full border' src={avatar} alt="" width={250} height={250}/>
+              </div>
+          </div>
+        </div>
+
+        <div className='container h-1/2 mx-auto'>
+            <div className='flex justify-center mt-24 mb-4'>
+                <img className='w-3/4 h-3/4' src={device} alt=""/>
             </div>
         </div>
     </>
