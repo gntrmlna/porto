385a16b2c54a16d251edca988c3c4169b9021937
Guntur <gntrmlnafrds@gmail.com>
2023-01-09 15:11:19 +0700

    benerin UI mobile, logo, font

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import logo from './Pic/logo.png'
 export default function Navbar() {
   return (
     <header className="text-gray-600 body-font">
@@ -11,11 +11,13 @@ export default function Navbar() {
             <a className="hover:text-gray-900">Fourth Link</a> */}
             </nav>
             <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
-                <button className="animate-button">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                    </svg>
+                <button className="flex justify-center mt-4">
+                    
+                    <img className='animate-button w-14 lg:w-16' src={logo} alt="" />
+                    
                 </button>
+                
+                
             {/* <span className="ml-3 text-xl">Guntur</span> */}
             </a>
             <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
--- src/Components/contentWelcome.jsx
+++ src/Components/contentWelcome.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import avatar from './avatar.png'
-import device from './device.png'
+import avatar from './Pic/avatar.png'
+import device from './Pic/device.png'
 export default function Welcome() {
   return (
     <>
-        <div className='container h-1/2 mx-auto'>
-          <div className='text-center mt-12'>
-              <h1 className='text-4xl font-bold'>Front End Developer</h1>
-              <h1 className='text-2xl mt-8'>I love simple things, and i love what i do.</h1>
-              <div className='flex justify-center mt-20'>
-                  <img className='rounded-full border' src={avatar} alt="" width={250} height={250}/>
+        <div className='container mx-auto'>
+          <div className='text-center mt-2 lg:mt-12'>
+              <h1 className='text-2xl lg:text-4xl font-bold'>Front End Developer</h1>
+              <h1 className='text-lg lg:text-2xl mt-4 lg:mt-8'>I love simple things, and i love what i do.</h1>
+              <div className='flex justify-center mt-12 lg:mt-20'>
+                  <img className='rounded-full border lg:w-2/12' src={avatar} alt="avatar"/>
               </div>
           </div>
         </div>
 
         <div className='container h-1/2 mx-auto'>
             <div className='flex justify-center mt-24 mb-4'>
-                <img className='w-3/4 h-3/4' src={device} alt=""/>
+                <img className='w-11/12 h-11/12 lg:w-3/4 lg:h-3/4' src={device} alt="device"/>
             </div>
         </div>
     </>
