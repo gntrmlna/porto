ac80a4059bc86d19a5d31b0bc4bd2d0e08a25ad5
Guntur <gntrmlnafrds@gmail.com>
2023-01-31 11:28:14 +0700

    update foto project

--- src/Components/KontenPorto.jsx
+++ src/Components/KontenPorto.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import useTransition from 'react-transition-state';
+import kasokan from './Pic/kasokan.png';
+import smpn7 from './Pic/smpn7.png';
+import smpn3 from './Pic/smpn3.png';
 
 export default function KontenPorto() {
     const [{status,isMounted},toggle] = useTransition({timeout:750,preEnter:true});
@@ -18,7 +21,7 @@ export default function KontenPorto() {
               <section className="text-gray-600 body-font">
                 <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
                   <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-                    <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"/>
+                    <img className="object-cover object-center rounded" alt="hero" src={kasokan}/>
                   </div>
                   <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Company Profile 
@@ -52,7 +55,7 @@ export default function KontenPorto() {
                     </div>
                   </div>
                   <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                    <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"/>
+                    <img className="object-cover object-center rounded" alt="hero" src={smpn7}/>
                   </div>
                 </div>
               </section>
@@ -60,7 +63,7 @@ export default function KontenPorto() {
               <section className="text-gray-600 body-font">
                 <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
                   <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-                    <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"/>
+                    <img className="object-cover object-center rounded" alt="hero" src={smpn3}/>
                   </div>
                   <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Website Sekolah
